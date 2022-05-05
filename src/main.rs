2b0b8c971931c66875c806baa7a2e41445d7efb4
Wasin Thonkaew <haxpor@gmail.com>
2022-05-05 19:11:39 +0700

    evmscan migration: migrate to use evmscan to support other major EVM-based chains
    Now it supports BSC, Ethereum, and Polygon for the input address.
    
    src/types.rs, and src/util.rs are added to help the logic code, and
    abstract stuff better.
    
    Brought in clap, and regex crate as well to help in validating cli's
    arguments, and utility function to check validity of input address.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
-use ::bscscan::bscscan;
-use ::bscscan::environ::Context;
-use ::bscscan::prelude::*;
+mod types;
+mod util;
+
+use ::evmscan::evmscan;
+use ::evmscan::environ::Context;
+use ::evmscan::prelude::*;
+use clap::Parser;
+use types::*;
+use util::*;
 
 fn main() {
-	let args: Vec<String> = std::env::args().collect();
-	if args.len() < 2 {
-		eprintln!("Usage: {pkg_name} <target_address>", pkg_name=env!("CARGO_PKG_NAME", "inoutflow-bsc"));
-        std::process::exit(1);
-	}
+	let cmd_args = CommandlineArgs::parse();
 
-    let mut target_address = args[1].to_owned();
+    // validate value of chain flag option
+    let chain_value = cmd_args.chain.to_lowercase();
+    let mut chain: Option<ChainType> = None;
+    if chain_value == "bsc" {
+        chain = Some(ChainType::BSC);
+    }
+    else if chain_value == "ethereum" {
+        chain = Some(ChainType::Ethereum);
+    }
+    else if chain_value == "polygon" {
+        chain = Some(ChainType::Polygon);
+    }
+    // NOTE: no need for else case here as clap crate handles non-valid values
+    // for us.
+    
+    // validate the required argument; address
+    if !is_address_simplified(&cmd_args.address) {
+        eprintln!("Error: input address is malformed. Make sure to prefix with '0x' and has 40 characters in length (exclude `0x`).");
+        std::process::exit(1);
+    }
+
+    // make it ascii-lowercased for input address
+    let mut target_address = cmd_args.address.to_owned();
     target_address.make_ascii_lowercase();
 
-    // create bscscan's context
-    let ctx = Context { api_key: std::env::var("HX_INOUTFLOW_API_KEY").expect("required 'HX_INOUTFLOW_API_KEY' environment variable to be defined") };
+    // get token name
+    let native_token_name = get_native_token_name(chain.unwrap());
+
+    // create a context
+    let ctx = Context::create(chain.unwrap(), select_apikey(chain.unwrap()));
 
     // get normal transactions
     {
-        let txs_res = bscscan::accounts().get_list_normal_transactions(&ctx, target_address.as_str());
+        let txs_res = evmscan::accounts().get_list_normal_transactions(&ctx, target_address.as_str());
         if let Err(e) = txs_res {
             eprintln!("{}", e);
             std::process::exit(1);
@@ -28,19 +55,19 @@ fn main() {
                 println!("Found 0 txs!");
             }
             else {
-                let bnb_outflow: U256 = txs.iter().filter(|tx| (tx.from == target_address) && !tx.is_error)
+                let ntoken_outflow: U256 = txs.iter().filter(|tx| (tx.from == target_address) && !tx.is_error)
                     .fold(U256::zero(), |acc, tx| acc + tx.value);
-                let bnb_inflow: U256 = txs.iter().filter(|tx| (tx.to == target_address) && !tx.is_error)
+                let ntoken_inflow: U256 = txs.iter().filter(|tx| (tx.to == target_address) && !tx.is_error)
                     .fold(U256::zero(), |acc, tx| acc + tx.value);
 
-                let bnb_outflow_f = bnb_outflow.to_f64_lossy() / bscscan::BNB_SCALE_F;
-                let bnb_inflow_f = bnb_inflow.to_f64_lossy() / bscscan::BNB_SCALE_F;
+                let ntoken_outflow_f = ntoken_outflow.to_f64_lossy() / evmscan::NATIVE_TOKEN_SCALE_F;
+                let ntoken_inflow_f = ntoken_inflow.to_f64_lossy() / evmscan::NATIVE_TOKEN_SCALE_F;
 
                 // add feature "fp-conversion" for primitive-types crate to use to_f64_lossy()
                 println!("Found {} txs!", txs.len());
-                println!("- BNB outflow: {} BNBs", bnb_outflow_f);
-                println!("- BNB inflow: {} BNBs", bnb_inflow_f);
-                println!("- Net in/out balance: {} BNBs", bnb_inflow_f - bnb_outflow_f);
+                println!("- {} outflow: {} {}s", native_token_name, ntoken_outflow_f, native_token_name);
+                println!("- {} inflow: {} {}s", native_token_name, ntoken_inflow_f, native_token_name);
+                println!("- Net in/out balance: {} {}s", ntoken_inflow_f - ntoken_outflow_f, native_token_name);
             }
         }
     }
@@ -49,7 +76,7 @@ fn main() {
 
     // get internal transactions
     {
-        let txs_res = bscscan::accounts().get_list_internal_transactions(&ctx, target_address.as_str());
+        let txs_res = evmscan::accounts().get_list_internal_transactions(&ctx, target_address.as_str());
         if let Err(e) = txs_res {
             eprintln!("{}", e);
             std::process::exit(1);
@@ -60,19 +87,19 @@ fn main() {
                 println!("Found 0 internal txs!");
             }
             else {
-                let bnb_outflow: U256 = txs.iter().filter(|tx| tx.from == target_address && !tx.is_error)
+                let ntoken_outflow: U256 = txs.iter().filter(|tx| tx.from == target_address && !tx.is_error)
                     .fold(U256::zero(), |acc, tx| acc + tx.value);
-                let bnb_inflow: U256 = txs.iter().filter(|tx| tx.to == target_address && !tx.is_error)
+                let ntoken_inflow: U256 = txs.iter().filter(|tx| tx.to == target_address && !tx.is_error)
                     .fold(U256::zero(), |acc, tx| acc + tx.value);
 
-                let bnb_outflow_f = bnb_outflow.to_f64_lossy() / bscscan::BNB_SCALE_F;
-                let bnb_inflow_f = bnb_inflow.to_f64_lossy() / bscscan::BNB_SCALE_F;
+                let ntoken_outflow_f = ntoken_outflow.to_f64_lossy() / evmscan::NATIVE_TOKEN_SCALE_F;
+                let ntoken_inflow_f = ntoken_inflow.to_f64_lossy() / evmscan::NATIVE_TOKEN_SCALE_F;
 
                 // add feature "fp-conversion" for primitive-types crate to use to_f64_lossy()
                 println!("Found {} internal txs!", txs.len());
-                println!("- BNB outflow: {} BNBs", bnb_outflow_f);
-                println!("- BNB inflow: {} BNBs", bnb_inflow_f);
-                println!("- Net in/out balance: {} BNBs", bnb_inflow_f - bnb_outflow_f);
+                println!("- {} outflow: {} {}s", native_token_name, ntoken_outflow_f, native_token_name);
+                println!("- {} inflow: {} {}s", native_token_name, ntoken_inflow_f, native_token_name);
+                println!("- Net in/out balance: {} {}s", ntoken_inflow_f - ntoken_outflow_f, native_token_name);
             }
         }
     }
@@ -84,14 +111,14 @@ fn main() {
     // from in/out flow of normal and internal transactions, and gas fees in `normal` transactions.
     // See README.md at `Technical Tips` section for more detail.
     {
-        let balance_res = bscscan::accounts().get_balance_address(&ctx, target_address.as_str());
+        let balance_res = evmscan::accounts().get_balance_address(&ctx, target_address.as_str());
         if let Err(e) = balance_res {
             eprintln!("{}", e);
             std::process::exit(1);
         }
 
         if let Ok(balance) = balance_res {
-            println!("Total balance: {} BNBs", balance.to_f64_lossy() / bscscan::BNB_SCALE_F);
+            println!("Total balance: {} {}s", balance.to_f64_lossy() / evmscan::NATIVE_TOKEN_SCALE_F, native_token_name);
         }
     }
 }
